37b0977cff9369ce9fb1a46ed88b37561cedc6db
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-18 00:37:04 +0800

    ``` feat(core): 使用Path类替代字符串处理文件路径
    引入base::Path类型来更好地管理文件路径，替换原有的std::string类型。
    通过使用ToAbsolutePath函数将输入路径转换为绝对路径，
    增强了路径处理的可靠性和一致性。
    
    BREAKING CHANGE: API中的路径参数类型从std::string改为base::Path
    ```

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "base/filesystem/filesystem.h"
+#include "base/filesystem/Path.h"
 #include "CLI/CLI.hpp"
 #include <iostream>
 #include <string>
@@ -7,17 +9,20 @@ namespace
 	class Parser
 	{
 	private:
-		std::string _src_path{};
-		std::string _dst_path{};
+		base::Path _src_path{};
+		base::Path _dst_path{};
 
 	public:
 		Parser(int argc, char **argv)
 		{
 			CLI::App app{"如实地拷贝目录或文件。对于目录，会递归拷贝，其中的符号链接会仍然保持为符号链接。"};
 
+			std::string src_path_string{};
+			std::string dst_path_string{};
+
 			{
 				CLI::Option *src_path_option = app.add_option("--src",
-															  _src_path,
+															  src_path_string,
 															  "源路径");
 
 				src_path_option->required();
@@ -25,21 +30,27 @@ namespace
 
 			{
 				CLI::Option *dst_path_option = app.add_option("--dst",
-															  _dst_path,
+															  dst_path_string,
 															  "目标路径");
 
 				dst_path_option->required();
 			}
 
 			app.parse(argc, argv);
+
+			_src_path = src_path_string;
+			_src_path = base::filesystem::ToAbsolutePath(_src_path);
+
+			_dst_path = dst_path_string;
+			_dst_path = base::filesystem::ToAbsolutePath(_dst_path);
 		}
 
-		std::string const &SrcPath() const
+		base::Path const &SrcPath() const
 		{
 			return _src_path;
 		}
 
-		std::string const &DstPath() const
+		base::Path const &DstPath() const
 		{
 			return _dst_path;
 		}
