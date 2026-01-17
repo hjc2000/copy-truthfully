269986bec0a688da9f9b8445d5f4036accba2a9d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-01-18 00:32:55 +0800

    ``` feat(cli): 添加CLI参数解析功能
    - 在project.cmake中添加对CLI11库的依赖引入
    - 实现Parser类来处理命令行参数解析
    - 支持--src和--dst两个必需参数用于指定源路径和目标路径
    - 使用CLI11库提供友好的命令行界面
    ```

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,58 @@
-#include <cstdint>
+#include "CLI/CLI.hpp"
 #include <iostream>
+#include <string>
 
-int main()
+namespace
 {
+	class Parser
+	{
+	private:
+		std::string _src_path{};
+		std::string _dst_path{};
+
+	public:
+		Parser(int argc, char **argv)
+		{
+			CLI::App app{"如实地拷贝目录或文件。对于目录，会递归拷贝，其中的符号链接会仍然保持为符号链接。"};
+
+			{
+				CLI::Option *src_path_option = app.add_option("--src",
+															  _src_path,
+															  "源路径");
+
+				src_path_option->required();
+			}
+
+			{
+				CLI::Option *dst_path_option = app.add_option("--dst",
+															  _dst_path,
+															  "目标路径");
+
+				dst_path_option->required();
+			}
+
+			app.parse(argc, argv);
+		}
+
+		std::string const &SrcPath() const
+		{
+			return _src_path;
+		}
+
+		std::string const &DstPath() const
+		{
+			return _dst_path;
+		}
+	};
+
+} // namespace
+
+int main(int argc, char **argv)
+{
+	Parser parser{argc, argv};
+
+	std::cout << "源路径：" << parser.SrcPath() << std::endl;
+	std::cout << "目标路径：" << parser.DstPath() << std::endl;
+
 	return 0;
 }
